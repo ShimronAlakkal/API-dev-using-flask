250f15d496fd5f2307ed162cbd02b966a64dc050
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-01-26 12:20:35 +0530

    starting to work on the api

--- database.py
+++ database.py
@@ -1,2 +1,4 @@
 # making an api to test notekeeping #classicbegineerprojects
 
+def user_exists(uid : int):
+    return True
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI, HTTPException
+import string
+from telnetlib import STATUS
+from pydantic import BaseModel
+from typing import Optional
+from fastapi import FastAPI, HTTPException, status
+from .database import *
 
 app = FastAPI()
+
+class Notes(BaseModel):
+    title : string
+    description : string
 
 @app.get("/")
 def index():
     return {'message':"you're at ques notes"}
+
+# get user profile 
+@app.get("/users/{uid}",status_code = status.HTTP_200_OK)
+async def get_user_profile(uid :int):
+    
+    if user_exists(uid) : 
+        return {'User':{uid:'info goes here'}}
+        # get the user details from db and retunr it
+    else:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"A user with id = {uid} could not be found")
+
