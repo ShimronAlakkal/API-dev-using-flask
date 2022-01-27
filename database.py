688d62e1fd2106105ac6890c3507b537f346dbbb
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-01-27 12:22:36 +0530

    bug in delete method

--- database.py
+++ database.py
@@ -1,4 +1,72 @@
-# making an api to test notekeeping #classicbegineerprojects
+import mysql.connector as mc
+from starlette.status import HTTP_404_NOT_FOUND
 
-def user_exists(uid : int):
-    return True
+connection = mc.connect( host = "localhost", passwd = "qwertyuiopasdfghjkl" ,user = "root",database = "api")
+cursor = connection.cursor()
+cursor.execute('use api;')
+
+# insert new record 
+# if you want to pass in a null value, use 'null' in the value holder.
+def inster_to_table(record : dict):
+    global cursor
+    cursor.execute(f"INSERT INTO video_db(title,likes,comments) VALUES ( '{record['title']}', {record['likes']}, {record['comments']} )")
+    connection.commit()
+    return 201
+    
+    
+# retreiev all data
+def get_all():
+    global cursor
+    v = []
+    cursor.execute('select * from video_db')
+    for i in cursor:
+        v.append(i)
+    return 200,v
+
+
+# get one data
+def get_one_specific_data(vid):
+    global cursor
+    v = []
+    cursor.execute(f'select * from video_db where vid = {vid}')
+    for i in cursor:
+        v.append(i)
+    if len(v)>0:
+        return v
+    return 404
+
+# update an existing data
+async def update_video_info(vid,record):
+    global cursor, connection
+    try:
+        cursor.execute(f"UPDATE video_db set  title =  '{record['title']}', likes = {record['likes']}, comments = {record['comments']} where vid = {vid}")
+        connection.commit()
+        return 202
+    except:
+        return 404
+
+    
+
+# delete an existing video
+async def delete_video(vid):
+    global connection,cursor
+    try:
+        cursor.execute(f"DELETE from video_db where vid = {vid}")
+        connection.commit()
+    except:
+        raise HTTP_404_NOT_FOUND
+
+    
+
+# a func to check if video exists or not in the db
+def find_video_with_id(vid):
+    global cursor
+    cursor.execute(f'SELECT 1 FROM video_db WHERE vid = {vid}')
+    for i in cursor:
+        if i[0] == 1:
+            return True
+    return False
+
+
+
+  
--- main.py
+++ main.py
@@ -1,27 +1,75 @@
-import string
-from telnetlib import STATUS
-from pydantic import BaseModel
+from os import stat
 from typing import Optional
-from fastapi import FastAPI, HTTPException, status
-from .database import *
+from fastapi import FastAPI, status, HTTPException
+from pydantic import BaseModel
+from database import *
+
+#this is some sort of video storing app api
 
 app = FastAPI()
 
-class Notes(BaseModel):
-    title : string
-    description : string
+class VideoInfo(BaseModel):
+    title: str
+    likes: int
+    comments: Optional[int] = "null"
 
+# The home route 
 @app.get("/")
 def index():
-    return {'message':"you're at ques notes"}
+    return {"message": "You're on Ques"}
 
-# get user profile 
-@app.get("/users/{uid}",status_code = status.HTTP_200_OK)
-async def get_user_profile(uid :int):
-    
-    if user_exists(uid) : 
-        return {'User':{uid:'info goes here'}}
-        # get the user details from db and retunr it
+
+
+# get all videos in app
+@app.get("/videos",status_code = status.HTTP_200_OK)
+async def get_all_videos():
+    status, values = get_all()
+    return {'data':values}
+
+
+
+# create a new video that is not already in the db
+@app.post('/videos',status_code = status.HTTP_201_CREATED)
+async def create_video(vi: VideoInfo):
+    status  = inster_to_table( vi.dict() )
+    if status != 201:
+        raise HTTPException(status_code = status.HTTP_208_ALREADY_REPORTED,detail = 'The given video already exist' )
     else:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"A user with id = {uid} could not be found")
+        return {'status': 'Created new video successfully'}
 
+
+
+# get video info of a certain video provided its id
+@app.get('/videos/{vid}',status_code = status.HTTP_200_OK)
+async def video_info_on_id(vid : int):
+    values = get_one_specific_data(vid)
+    if values != 404:
+        return {'data': values}
+    elif values == 404:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f"a video with id {vid} was not found")
+
+
+
+
+# updating the details of an existing video
+@app.put('/videos/{vid}',status_code = status.HTTP_202_ACCEPTED)
+async def update_video_details(vid : int, updation_info : VideoInfo):
+    if find_video_with_id(vid):
+        record = updation_info.dict()
+        state = await update_video_info(vid,record)
+    else:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f'Could not find a video with id = {vid}')
+
+    if state == 202:
+        return {vid:record}
+
+
+
+
+# deleting a video that exists 
+@app.delete('/videos/{vid}',status_code = status.HTTP_204_NO_CONTENT)
+async def delete_video(vid: int):
+    if find_video_with_id(vid):
+        await delete_video(vid)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Found no video with id = {vid}')
